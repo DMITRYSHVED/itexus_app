4094687333360997c17f2f18d9e01c04fa19c540
DMITRYSHVED <dima.shved.0202@mail.ru>
2024-03-27 14:02:46 +0300

    commit

---
+++ src/test/java/com/example/miracles_store/MiraclesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miracles_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraclesStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
